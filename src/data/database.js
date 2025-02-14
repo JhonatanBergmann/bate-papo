698cffa9d0e38a7c693b921db5fe2fc44ce8edc0
Jhonatan Bergmann <54135650+jhonbergmann@users.noreply.github.com>
2025-02-14 20:44:33 -0300

    database

--- src/data/database.js
+++ src/data/database.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDy02JkMl_XAymzYAajWlI37hagsbZFl7A",
-  authDomain: "chatapp-384bb.firebaseapp.com",
-  databaseURL: "https://chatapp-384bb.firebaseio.com",
-  projectId: "chatapp-384bb",
-  storageBucket: "chatapp-384bb.appspot.com",
-  messagingSenderId: "462796692755",
-  appId: "1:462796692755:web:c0b7d195e421800568cee6"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 }
 
 firebase.initializeApp(firebaseConfig)
